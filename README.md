41afe2397c1773e587bd16b6e12b28db29e9df62
Jesper Andersen <jespera@gmail.com>
2019-03-21 10:00:59 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # sic
-NPM package for getting the `sic` binary
+
+This is the NPM package for getting the binary of the `sic` code generation tool.
+
+# License
+
+This particular wrapper for getting `sic` is licensed under the AGPLv3 license as given in `AGPL` while the `sic` binary has a custom license as given in `LICENSE.txt`.
---
+++ index.js
@@ -0,0 +1,16 @@
+var binwrap = require("binwrap");
+var path = require("path");
+
+var packageInfo = require(path.join(__dirname, "package.json"));
+var version = packageInfo.version;
+var root = "https://files.dev.deon.digital/sic/" + "v" + version;
+
+module.exports = binwrap({
+  dirname: __dirname,
+  binaries: [ "sic" ],
+  urls: {
+    "darwin-x64": root + "/macos/sic.tar.gz",
+    "linux-x64": root + "/linux/sic.tar.gz",
+    "win32-x64": root + "/windows/sic.zip"
+  }
+});
